7f51278c1c1fad507bc48ed4f6615d19944a65c8
Andrew Mascillaro <amascillaro@gmail.com>
2021-09-23 11:01:48 -0400

    Template live data selection

--- plotter.py
+++ plotter.py
@@ -5,16 +5,33 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 data = pd.read_csv("points.csv")
+num_points = 0
 
 while True:
 	if data.shape[0] >= 4:
+		num_points = data.shape[0]
 		break
 	time.sleep(1)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection="3d")
 
-# while 
 ax.plot_trisurf(data["x"], data["y"], data["z"])
 
-plt.show()
+plt.show() # remove ???
+
+'''
+while True:
+	data = pd.read_csv("points.csv")
+	if num_points < data.shape[0]:
+		num_points = data.shape[0]
+
+		# replot points
+		ax.set_xdata(data["x"]) ???
+		??? y, z ???
+
+		# redraw surface
+		ax.draw() ????
+
+'''
+
