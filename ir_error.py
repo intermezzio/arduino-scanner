31d2b36a512c1ee8506869bf8c2f0cf91b482661
Andrew Mascillaro <amascillaro@gmail.com>
2021-09-27 11:20:32 -0400

    Make plots look prettier

---
+++ ir_error.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+IR_ERROR = 11166.750185583001 # precalculated coefficient
+ir_func = lambda x: IR_ERROR/x
+
+ir_data = pd.read_csv("ir_error.csv")
+fig, ax = plt.subplots()
+
+ax.plot(ir_data["actual"], ir_data["expected"],
+	"r.", label="observed data")
+
+input_values = np.linspace(30,60,300)
+ax.plot(input_values, input_values, "k--",
+	label="f(x) = x")
+
+ax.set_xlabel("Actual Distance (cm)")
+ax.set_ylabel("Distance Reading (cm)")
+ax.set_title("Error plot for IR Sensor Readings")
+
+fig.legend(loc="center right")
+fig.savefig("ir_error.png")
+
+plt.show()
