38991e49645b72ada525df4debe1acd17d9f4718
Andrew Mascillaro <amascillaro@gmail.com>
2021-09-29 10:34:40 -0400

    Fix receiver variable names and plot live data

--- plotter.py
+++ plotter.py
@@ -1,18 +1,21 @@
-import numpy as np
 import pandas as pd
 import time
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import seaborn as sns
+
+sns.set(style="whitegrid")
 
 data = pd.read_csv("points.csv")
 num_points = 0
 
-while True:
-	if data.shape[0] >= 4:
-		num_points = data.shape[0]
-		break
-	time.sleep(1)
+# while True:
+# 	if data.shape[0] >= 4:
+# 		num_points = data.shape[0]
+# 		break
+# 	print("Not enough points to start plotting")
+# 	time.sleep(1)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection="3d")
@@ -20,13 +23,19 @@ ax = fig.add_subplot(111, projection="3d")
 scat = ax.scatter(data["x"], data["y"], data["z"])
 # surf = ax.plot_trisurf(data["x"], data["y"], data["z"])
 
+ax.set_xlabel("x distance (cm)")
+ax.set_ylabel("y distance (cm)")
+ax.set_zlabel("z distance (cm)")
+
+ax.set_title("IR Scan Data")
+
 plt.ion()
 plt.show()
 
 while True:
 	data = pd.read_csv("points.csv")
 	print(data.shape)
-	if True or num_points < data.shape[0]:
+	if num_points < data.shape[0]:
 		num_points = data.shape[0]
 
 		# replot points
@@ -35,6 +44,13 @@ while True:
 		# scat.set_3d_properties(np.c_[data["x"],data["y"]],data["z"].to_numpy())
 		ax.clear()
 		scat = ax.scatter(data["x"], data["y"], data["z"])
+
+		ax.set_xlabel("x distance (cm)")
+		ax.set_ylabel("y distance (cm)")
+		ax.set_zlabel("z distance (cm)")
+
+		ax.set_title("IR Scan Data")
+		
 		plt.pause(1)
 		# redraw surface
 		fig.canvas.draw()
--- receiver.py
+++ receiver.py
@@ -1,20 +1,19 @@
 import serial
-from math import sind, cosd
 
-arduinoComPort = "COM6"
+arduino_com_port = "COM6"
 
-baudRate = 115200
+baud_rate = 9600
 
-serialPort = serial.Serial(arduinoComPort, baudRate, timeout=1)
+serial_port = serial.Serial(arduino_com_port, baud_rate, timeout=1)
 
 with open("points.csv", "w") as outfile:
 	outfile.write("x,y,z\n")
 
-with open("points.csv", "a") as outfile:
-	while True:
-		data = serialPort.readline().decode():
+while True:
+	data = serial_port.readline().decode()
 
-		if len(data):
-			distance, x_angle, y_angle, x_pos, y_pos, z_pos = [float(x) for x in data.split()]
+	if len(data):
+		distance, x_angle, y_angle, x_pos, y_pos, z_pos = [float(x) for x in data.split()]
 
+		with open("points.csv", "a") as outfile:
 			outfile.write(f"{x_pos},{y_pos},{z_pos}\n")
