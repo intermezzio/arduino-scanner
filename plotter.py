621202d2469633a70c081fca8a9ed1271cbdd3f7
Andrew Mascillaro <amascillaro@gmail.com>
2021-09-30 17:34:25 -0400

    Comment python code

--- plotter.py
+++ plotter.py
@@ -6,47 +6,49 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 import seaborn as sns
 
+# simple styling
 sns.set(style="whitegrid")
 
+# read data
 data = pd.read_csv("points.csv")
 num_points = 0
 
-# while True:
-# 	if data.shape[0] >= 4:
-# 		num_points = data.shape[0]
-# 		break
-# 	print("Not enough points to start plotting")
-# 	time.sleep(1)
+# wait until at least one point has been sent
+while True:
+	if data.shape[0] != 0:
+		num_points = data.shape[0]
+		break
+	print("Not enough points to start plotting")
+	time.sleep(1)
 
+# create plot
 fig = plt.figure()
 ax = fig.add_subplot(111, projection="3d")
 
 # colormap credit to https://stackoverflow.com/a/28604247/12940893
+# color points more red if they're closer to the sensor
 scat = ax.scatter(data["x"], data["y"], data["z"], facecolors=cm.coolwarm_r(data["y"]/data["y"].max()))
-# surf = ax.plot_trisurf(data["x"], data["y"], data["z"])
-# m = cm.ScalarMappable(cmap=cm.coolwarm_r)
-# m.set_array(data["y"])
-# cbar = plt.colorbar(m)
 
+# set axis labels and title
 ax.set_xlabel("x distance (cm)")
 ax.set_ylabel("y distance (cm)")
 ax.set_zlabel("z distance (cm)")
 
 ax.set_title("IR Scan Data")
 
+# set interactive plot for live data
 plt.ion()
 plt.show()
 
 while True:
+	# check for points every second
 	data = pd.read_csv("points.csv")
-	print(data.shape)
+	
+	# if there are more points than before
 	if num_points != data.shape[0]:
 		num_points = data.shape[0]
 
 		# replot points
-		# surf.set_verts(list(zip(data["x"], data["y"], data["z"])))
-		# scat.set_offsets(list(zip(data["x"], data["y"], data["z"])))
-		# scat.set_3d_properties(np.c_[data["x"],data["y"]],data["z"].to_numpy())
 		ax.clear()
 		scat = ax.scatter(data["x"], data["y"], data["z"], facecolors=cm.coolwarm_r(data["y"]/data["y"].max()))
 
@@ -61,4 +63,5 @@ while True:
 		fig.canvas.draw()
 
 	time.sleep(1)
-	
+
+plt.ioff()
--- receiver.py
+++ receiver.py
@@ -1,26 +1,33 @@
 import serial
 import csv
 import time
+
+# set ports and other constants for collecting data
 arduino_com_port = "/dev/cu.usbmodem143101"
 
 baud_rate = 9600
 
 serial_port = serial.Serial(arduino_com_port, baud_rate, timeout=1)
 
+# clear existing points.csv file
 with open("points.csv", "w") as outfile:
 	outfile.write("x,y,z\n")
 
 while True:
+	# every second check for serial data
 	time.sleep(1)
 	data = serial_port.readline().decode()
 
+	# check how many points are already recorded
 	file = open("points.csv")
 	reader = csv.reader(file)
 	lines= len(list(reader))
 
+	# if data has been sent via Serial
 	if len(data):
 		distance, x_angle, y_angle, x_pos, y_pos, z_pos = [float(x) for x in data.split()]
 
+		# append point iff there aren't too many points already
 		if lines < 443:
 			with open("points.csv", "a") as outfile:
 				outfile.write(f"{x_pos},{y_pos},{z_pos}\n")
